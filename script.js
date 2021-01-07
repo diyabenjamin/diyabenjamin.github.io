04aab5b75b019a80ce30f869a91df0a916e73f3f
Diya Benjamin <pramodiya@Pramods-MacBook-Pro.local>
2021-01-07 15:22:03 -0500

    Added gradient background and updated font styles

---
+++ script.js
@@ -0,0 +1,21 @@
+// let date = new Date();
+// document.getElementsByClassName("date").innerHTML = "<h4>Today's Date: " + date + "</h4>";
+//
+// function getSelectedValues() {
+//     let jobTypeRadios = document.getElementsByName('JobType');
+//     let locationRadios = document.getElementsByName('Location');
+//     for (let i = 0, length = jobTypeRadios.length; i < length; i++) {
+//         if (jobTypeRadios[i].checked) {
+//             console.log(jobTypeRadios[i].value);
+//             document.getElementById("selectjobtype").innerHTML = "Selected Job Type: "+ jobTypeRadios[i].value;
+//             break;
+//         }
+//     }
+//     for (let i = 0, length = locationRadios.length; i < length; i++) {
+//         if (locationRadios[i].checked) {
+//             console.log(locationRadios[i].value);
+//             document.getElementById("selectlocation").innerHTML = "Selected Location: "+ locationRadios[i].value;
+//             break;
+//         }
+//     }
+// }
